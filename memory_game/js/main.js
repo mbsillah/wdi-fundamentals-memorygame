f629bb4e6270d663248c197ed5419ca9e0f43ff9
Musa Sillah <macktastic2014@gmail.com>
2017-08-22 14:13:37 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,26 @@
-console.log("Up and running");
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match");
-	} else {
-		alert("Sorry, try again");
-	}
+			alert("You found a match");
+		} else {
+			alert("Sorry, try again");
+		}
+
 };
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardID) {
+
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+		}
+};
+
+flipCard(0);
+flipCard(2);
+
+
